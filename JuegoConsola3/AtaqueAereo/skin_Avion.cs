9920f2179891b0a9dcaedfddfb8eb08aec9c0813
Oscar Bautista <oscar04262000@gmail.com>
2020-05-05 15:44:09 -0500

    Ahora solo nuestro avion solo se puede mover dentro del area sin salirse de ella, creamos la clase Armas_Fuego para que nuestro avion pueda atacar

--- JuegoConsola3/AtaqueAereo/skin_Avion.cs
+++ JuegoConsola3/AtaqueAereo/skin_Avion.cs
@@ -8,31 +8,86 @@ namespace AtaqueAereo
     {
         private void dibuja_Avion()
         {
+            Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine("═╞");
+        }
+        private void campoBlanco()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("■■");
         }
         public void avion()
         {
             int x = 5;
             int y = 10;
+            
             ConsoleKeyInfo keyInfo;
             do
             {
                 Console.SetCursorPosition(x, y);
                 dibuja_Avion();
                 keyInfo = Console.ReadKey();
-
-                while (keyInfo.Key == ConsoleKey.UpArrow)
+                
+                while (keyInfo.Key == ConsoleKey.UpArrow) //Tecla arriba
                 {
                     y -= 1;
-                    Console.ForegroundColor = ConsoleColor.White;
                     Console.SetCursorPosition(x, y);
                     dibuja_Avion();
+                    Console.SetCursorPosition(x, y + 1);
+                    campoBlanco();
+                    
+                    if (y==0)
+                    {
+                        y += 1;
+                    }
+                    break;
+                }
+
+                while (keyInfo.Key == ConsoleKey.DownArrow) //Tecla abajo
+                {
+                    y += 1;
+                    Console.SetCursorPosition(x, y);
+                    dibuja_Avion();
+                    Console.SetCursorPosition(x, y - 1);
+                    campoBlanco();
+                    if (y==20)
+                    {
+                        y -= 1;
+                    }
+                    break;
+                }
+
+                while (keyInfo.Key == ConsoleKey.LeftArrow)// Tecla Izquierda
+                {
+                    x -= 1;
+                    Console.SetCursorPosition(x, y);
+                    dibuja_Avion();
+                    Console.SetCursorPosition(x+1, y);
+                    campoBlanco();
+                    if (x==0)
+                    {
+                        x += 1;
+                    }
+                    break;
+                }
+
+                while (keyInfo.Key == ConsoleKey.RightArrow) //Tecla Derecha
+                {
+                    x += 1;
+                    Console.SetCursorPosition(x, y);
+                    dibuja_Avion();
+                    Console.SetCursorPosition(x - 1, y);
+                    campoBlanco();
+                    if (x==60)
+                    {
+                        x -= 1;
+                    }
                     break;
                 }
                 
             } while (true);
             
-        }
+        } 
 
     }
 }
