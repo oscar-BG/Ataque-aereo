2fd81acd9ea6e01d228bc3ab5c70024288536af7
Oscar Bautista <oscar04262000@gmail.com>
2020-05-10 17:21:13 -0500

    Se crear la clase enemigos para que pueden interactuar con nuestro avion y este mismo pueda dispararles

---
+++ JuegoConsola3/AtaqueAereo/Enemigos.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtaqueAereo
+{
+    class Enemigos
+    {
+        private int c;
+        private int longitud;
+        
+        private void sking_enemigo()
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("╣");
+        }
+        private void campoBlanco()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("X");
+        }
+        private void colocarEnemigo()
+        {
+
+        }
+        public void enemigos(int suma)
+        {
+            Random random = new Random();
+            c = suma;
+            longitud = 60 - c;
+            int longBlanco = longitud + 1;
+            
+            int y = random.Next(1,20);
+            if (longitud > 0) 
+            {
+                Console.SetCursorPosition(longitud, y);
+                sking_enemigo();
+                for (int a = 0; a < 100000; a++)
+                {
+
+                }
+                Console.SetCursorPosition(longBlanco, y);
+                campoBlanco();
+
+                Console.SetCursorPosition(longitud, 10);
+                sking_enemigo();
+                for (int a = 0; a < 100000; a++)
+                {
+
+                }
+                Console.SetCursorPosition(longBlanco, 10);
+                campoBlanco();
+
+                Console.SetCursorPosition(longitud, 17);
+                sking_enemigo();
+                for (int a = 0; a < 100000; a++)
+                {
+
+                }
+                Console.SetCursorPosition(longBlanco, 17);
+                campoBlanco();
+            }
+        }
+    }
+}
